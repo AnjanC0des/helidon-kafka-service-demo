5dd013e8b8866445bc434e52770a2bf92da22651
AnjanC0des <129589930+AnjanC0des@users.noreply.github.com>
2025-02-12 15:51:56 +0000

    add confluent parallel consumer

--- helidonKafka/src/main/java/com/example/myproject/ConsumerService.java
+++ helidonKafka/src/main/java/com/example/myproject/ConsumerService.java
@@ -1,65 +1,109 @@
 package com.example.myproject;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import io.confluent.parallelconsumer.ParallelConsumerOptions;
+import io.confluent.parallelconsumer.ParallelStreamProcessor;
+import io.confluent.parallelconsumer.RecordContext;
+//import lombok.Value;
+//import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.concurrent.CircuitBreakingException;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.time.Duration;
-import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static io.confluent.csid.utils.StringUtils.msg;
+import static io.confluent.parallelconsumer.ParallelConsumerOptions.ProcessingOrder.KEY;
+import static pl.tlinkowski.unij.api.UniLists.of;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import java.lang.InterruptedException;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 public class ConsumerService {
-    private static volatile boolean running = true;
-    public static void run()  {
-	System.out.println("Consumer Started");
-	String bootstrapserver="127.0.0.1:9092";
-		//String topic="signal";
-		String group="consumergroup";
-		Properties props=new Properties();
-		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapserver);
+    static final String inputTopic = "messageTopic";
+    //String outputTopic = "output-topic-" + RandomUtils.nextInt();
+
+    static Consumer<String, String> getKafkaConsumer() {
+	    String bootstrapserver="127.0.0.1:9092";
+	    String group="consumergroup";
+	    Properties props=new Properties();
+	    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapserver);
 		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG,group);
+		props.setProperty("enable.auto.commit", "false");
 		props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
 		props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
 		props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-		consumer.subscribe(Collections.singletonList("messageTopic"));
+        return new KafkaConsumer<>(props);
+    }
 
-	        // Register shutdown hook
-        	Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            		running = false;
-			System.out.println("Shutdown triggered");
-            //		throw new InterruptedException("SHUTDOWN");
-       		 }));
+    static Producer<String, String> getKafkaProducer() {
+	    String bootstrapserver="127.0.0.1:9092";
+	    Properties props=new Properties();
+	    props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapserver);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+	//	props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new KafkaProducer<>(props);
+    }
 
-        	for (int i = 0; i < 10; i++) { // Start with 10 consumers
-            		Thread.ofVirtual().start(() -> {
+    static ParallelStreamProcessor<String, String> parallelConsumer;
 
-                		try {
-                   			while (running) {
-						ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                        			records.forEach(record -> {
-                            			// Publish to Redis cache
-                            			System.out.printf("Consumed record with key %s and value %s%n", record.key(), record.value());
-                        			});
-                    			}
-                		} catch (WakeupException e) {
-                    		// Ignore if shutting down
-                    			if (running) throw e;
-                		} finally {
-                    			consumer.close();
-                   			 System.out.println("Consumer closed.");
-                		}
-            		});
-        	}
-		
-	}
+    @SuppressWarnings("UnqualifiedFieldAccess")
+    static void run() {
+        parallelConsumer = setupParallelConsumer();
+
+       // postSetup();
+
+        // tag::example[]
+        parallelConsumer.poll(record ->
+                System.out.println("Concurrently processing a record: "+record)
+        );
+        // end::example[]
+    }
+
+    protected void postSetup() {
+        // ignore
+    }
+
+    @SuppressWarnings({"FeatureEnvy", "MagicNumber"})
+    static ParallelStreamProcessor<String, String> setupParallelConsumer() {
+        // tag::exampleSetup[]
+        Consumer<String, String> kafkaConsumer = getKafkaConsumer(); // <1>
+        Producer<String, String> kafkaProducer = getKafkaProducer();
+
+        var options = ParallelConsumerOptions.<String, String>builder()
+                .ordering(KEY) // <2>
+                .maxConcurrency(1000) // <3>
+                .consumer(kafkaConsumer)
+                .producer(kafkaProducer)
+                .build();
+
+        ParallelStreamProcessor<String, String> eosStreamProcessor =
+                ParallelStreamProcessor.createEosStreamProcessor(options);
+
+        eosStreamProcessor.subscribe(of(inputTopic)); // <4>
+
+        return eosStreamProcessor;
+        // end::exampleSetup[]
+    }
+
+     void close() {
+        this.parallelConsumer.close();
+    }
 }
-
-
-
-
